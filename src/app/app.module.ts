0ca10acbda606aef6edb00226cd0bc47918f27a3
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-06-11 12:43:06 +0200

    master: home, about, scroll ssnap, header, etc...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
